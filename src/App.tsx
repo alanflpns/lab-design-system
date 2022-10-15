3d81c9a8ee20b01e30e758d581be3df74db04ef1
alanflp <alanflpns@gmail.com>
2022-10-15 19:24:25 -0300

    finish ds

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Envelope } from "phosphor-react";
+import {
+  TextInput,
+  TextInputInputProps,
+  TextInputRootProps,
+} from "./TextInput";
+
+export default {
+  title: "Components/TextInput",
+  component: TextInput.Root,
+  args: {
+    children: [
+      <TextInput.Icon>
+        <Envelope />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder="Type your e-mail address" />,
+    ],
+  },
+  argTypes: {},
+} as Meta<TextInputRootProps>;
+
+export const Default: StoryObj<TextInputInputProps> = {};
+export const WithoutIcon: StoryObj<TextInputInputProps> = {
+  args: {
+    children: <TextInput.Input placeholder="Type your e-mail address" />,
+  },
+};
